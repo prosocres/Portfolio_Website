9cac9319800e147f1007c5b7903d0756c8781648
prosocres <20094619+prosocres@users.noreply.github.com>
2023-07-23 21:31:12 -0400

    Adds box shadow to all boxes on click

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-let interestBox = document.getElementsByClassName("interest-box");
+let interestBox1 = document.getElementById("interest-box-1");
 
-const interestBoxClickHandler = item => {
-    item.style = 'background-color: blue'
+const interestBoxEventHandler = (   ) => {
+    interestBox1.style.boxShadow='10px 5px 5px red';
 }
 
-interestBox.addEventListener("click", interestBoxClickHandler)
+interestBox1.addEventListener("mouseover", interestBoxEventHandler)
+
+const interestBox = document.querySelectorAll('.interest-box');
+
+interestBox.forEach(box => {
+    box.addEventListener('click', function() {
+        box.style.boxShadow='10px 5px 5px orange';
+    })
+})
